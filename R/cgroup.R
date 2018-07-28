4e63e12a44f4a33ac6b2417e9c84071e17690703
agtriplett <d.triplett@icloud.com>
2018-07-28 19:43:21 -0400

    Update

--- R/cgroup.R
+++ R/cgroup.R
@@ -1,7 +1,38 @@
+#' Control Group Selection
+#'
+#' @param test.group A dataframe
+#' @param n A number
+#' @param matching A dataframe
+#' @return You have a long way to go
+#' @export
+
+cgroup <- function(data, test.ids = NULL, n = 1, match.var = NULL, group.var = NULL){
+
+  if (!requireNamespace("dplyr", quietly = TRUE)) {
+    stop("Package \"dplyr\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+
+  if (is.null(group.var)) {
+    data %>%
+      mutate(group.var = 1)
+  }
+
+  if (is.null(match.var)) {
+    stop("Matching variables are required for this function to work. Please add at least one matching variable")
+  }
+
+  if (is.null(test.ids)) {
+    stop("At least one test id is required for this function to work.")
+  }
 
 
 
 
-cgroup <- function(test.group = NULL, n = 1, matching = NULL){
-  return(NULL)
+
+
+
+
+
+  return(print(output))
 }

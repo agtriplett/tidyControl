a6862db18a3a463214a0887190518f7688c47bbc
agtriplett <d.triplett@icloud.com>
2018-07-28 22:03:59 -0400

    Update

--- R/cgroup.R
+++ R/cgroup.R
@@ -1,68 +1,78 @@
-#' Control Group Selection
+#' Control Group Matching
 #'
-#' @param test.group A dataframe
-#' @param n A number
-#' @param matching A dataframe
-#' @return You have a long way to go
+#' @param data
+#' @param test.ids
+#' @param n
+#' @param match.var
+#' @param group.var
+#' @param impute
+#' @param keep.match.vars
+#'
+#' @return
 #' @export
+#'
+#' @examples
+#'
+match <-  function(data,
+           test.ids = NULL,
+           n = 1,
+           match.var = NULL,
+           group.var = NULL,
+           impute = NULL,
+           keep.match.vars = FALSE) {
+    if (!requireNamespace("dplyr", quietly = TRUE)) {
+      stop("Package \"dplyr\" needed for this function to work. Please install it.",
+           call. = FALSE)
+    }
 
-cgroup <- function(data, test.ids = NULL, n = 1, match.var = NULL, group.var = NULL, impute = NULL){
-
-  if (!requireNamespace("dplyr", quietly = TRUE)) {
-    stop("Package \"dplyr\" needed for this function to work. Please install it.",
-         call. = FALSE)
-  }
-
-  if (!requireNamespace("tidyr", quietly = TRUE)) {
-    stop("Package \"tidyr\" needed for this function to work. Please install it.",
-         call. = FALSE)
-  }
-
-
-  if (is.null(group.var)) {
-    data %>%
-      mutate(group.var = 1)
-  }
-
-  if (is.null(match.var)) {
-    stop("Matching variables are required for this function to work. Please add at least one matching variable")
-  }
-
-  if (is.null(test.ids)) {
-    stop("At least one test id is required for this function to work.")
-  }
+    if (!requireNamespace("tidyr", quietly = TRUE)) {
+      stop("Package \"tidyr\" needed for this function to work. Please install it.",
+           call. = FALSE)
+    }
 
 
-  dq <- data %>%
-    group_by(id) %>%
-    summarise(count = n())
-  imp <- case_when(dq, sum(count<max(count)) > 0 ~ 1,
-                   TRUE ~ 0)
+    if (is.null(group.var)) {
+      data %>%
+        mutate(group.var = 1)
+    }
+
+    if (is.null(match.var)) {
+      stop(
+        "Matching variables are required for this function to work. Please add at least one matching variable"
+      )
+    }
+
+    if (is.null(test.ids)) {
+      stop("At least one test id is required for this function to work.")
+    }
 
 
+    dq <- data %>%
+      group_by(id) %>%
+      summarise(count = n())
+    imp <- case_when(dq, sum(count < max(count)) > 0 ~ 1,
+                     TRUE ~ 0)
 
-  if (is.null(impute) & imp == 1) {
-  stop("You have varying data points per id and have chosen not to impute missing values")
-  }
+    if (is.null(impute) & imp == 1) {
+      stop("You have varying data points per id and have chosen not to impute missing values")
+    }
 
-  if(!is.null(impute) & imp == 1){
-    data <- merge(
-      expand.grid(group=unique(data$group.var), time=unique(data$date)),
-      data, all=TRUE)
+    if (!is.null(impute) & imp == 1) {
+      data <- merge(expand.grid(group = unique(data$id),
+                                time = unique(data$date)),
+                    data,
+                    all = TRUE)
 
-    for(i in names(match.var)) {
-      data <- data %>%
-        replace_na(impute(i, na.rm = T))
+      for (i in names(match.var)) {
+        data <- data %>%
+          replace_na(impute(i, na.rm = T))
       }
+    }
+
+    outut <- 1
 
 
 
 
-
+      return(print(output))
   }
-
-
-
-
-  return(print(output))
-}
---
+++ R/plot.R
@@ -0,0 +1,42 @@
+#' Control Group Match Plotting
+#'
+#' @param output
+#' @param match.var
+#'
+#' @return A plot of each chosen matching variable grouped by test stores to check for match quality.
+#' @export
+#'
+#' @examples
+#'
+plot.control <- function(output,
+                         match.var = NULL,
+                         all.var = FALSE) {
+  if (is.null(match.var)) {
+    stop(
+      "Matching variables are required for this function to work. Please add at least one matching variable"
+    )
+  }
+
+  if (all.var == FALSE) {
+    output <- output %>%
+      mutate(id = as.factor(id)) %>%
+      filter(match.var == match.var)
+    ggplot(., x = timepoint, y = match.var, color = id) +
+      geom_point() +
+      geom_line()
+  }
+
+  if (all.var == TRUE) {
+    output <- output %>%
+      mutate(id = as.factor(id)) %>%
+      ggplot(., x = timepoint, y = match.var, color = id) +
+      geom_point() +
+      geom_line() +
+      facet_grid(. ~ match.var)
+  }
+
+
+
+
+
+}
